ed2241c8f118e9208ec43d15c227d7a6f607ee22
santos <249254963+santosgvdaw@users.noreply.github.com>
2025-12-15 17:07:44 +0100

    Revert "Merge pull request #2 from santosgvdaw/feature/sql"
    This reverts commit caf1e00fa88c85dc979ae8f3e865790ac75e9156, reversing
    changes made to 5ea53651e391907f60e5ceb4019ff546651f05e5.

--- scripts/3 - usuarios.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'almacen'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON `almacen_db`.* TO 'almacen'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
-CREATE USER 'usuario'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON `usuarios_db`.* TO 'usuario'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
