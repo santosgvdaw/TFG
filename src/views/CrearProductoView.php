b16810a1694bd9b56d6b3b084521e51e71646e19
santos <249254963+santosgvdaw@users.noreply.github.com>
2026-02-11 17:43:19 +0100

    chore: posicion error campos producto

--- src/views/CrearProductoView.php
+++ src/views/CrearProductoView.php
@@ -37,20 +37,19 @@ class CrearProductoView extends BaseView
         <form id="crear" name="crear" action="crearProducto.php" method="POST" style="max-width: 330px;">
             <div id="errores" class="row mb-3">
                 <div id="errorExiste" class="alert alert-danger <?= in_array('errorExiste', $this->error) ? '' : 'd-none' ?>" role="alert">Ya existe un producto con ese nombre</div>
-                <div id="errorNombre" class="alert alert-danger <?= in_array('errorNombre', $this->error) ? '' : 'd-none' ?>" role="alert">El nombre es demasiado largo (max. 20 caracteres) o corto (min. 1 caracter)</div>
-                <div id="errorDescripcion" class="alert alert-danger <?= in_array('errorDescripcion', $this->error) ? '' : 'd-none' ?>" role="alert">La descripción es demasiado larga (max. 255 caracteres) o corta (min. 1 caracter)</div>
-                <div id="errorCategoria" class="alert alert-danger <?= in_array('errorCategoria', $this->error) ? '' : 'd-none' ?>" role="alert">La categoría seleccionada no existe</div>
-                <div id="errorStockMinimo" class="alert alert-danger <?= in_array('errorStockMinimo', $this->error) ? '' : 'd-none' ?>" role="alert">El stock no es un número entero positivo</div>
             </div>
             <div class="row mb-3">
+                <div id="errorNombre" class="alert alert-danger <?= in_array('errorNombre', $this->error) ? '' : 'd-none' ?>" role="alert">El nombre es demasiado largo (max. 20 caracteres) o corto (min. 1 caracter)</div>
                 <label for="nombre" class="form-label">Nombre</label>
                 <input type="text" class="form-control" name="nombre" id="nombre" />
             </div>
             <div class="row mb-3">
+                <div id="errorDescripcion" class="alert alert-danger <?= in_array('errorDescripcion', $this->error) ? '' : 'd-none' ?>" role="alert">La descripción es demasiado larga (max. 255 caracteres) o corta (min. 1 caracter)</div>
                 <label for="descripcion" class="form-label">descripción</label>
                 <textarea class="form-control" name="descripcion" id="descripcion" rows="3"></textarea>
             </div>
             <div class="row mb-3">
+                <div id="errorCategoria" class="alert alert-danger <?= in_array('errorCategoria', $this->error) ? '' : 'd-none' ?>" role="alert">La categoría seleccionada no existe</div>
                 <label for="categoria" class="form-label">Categoria</label>
                 <select class="form-select" name="categoria" id="categoria">
                     <?php foreach ($this->categorias as $categoria) { ?>
@@ -60,6 +59,7 @@ class CrearProductoView extends BaseView
 
             </div>
             <div class="row mb-3">
+                <div id="errorStockMinimo" class="alert alert-danger <?= in_array('errorStockMinimo', $this->error) ? '' : 'd-none' ?>" role="alert">El stock no es un número entero positivo</div>
                 <label for="stockMinimo" class="form-label">Stock mínimo</label>
                 <input type="number" class="form-control" name="stockMinimo" id="stockMinimo" />
             </div>
